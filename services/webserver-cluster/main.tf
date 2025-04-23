0920722321acc1c1f1cdc61469022f3432349523
steph-nnamani@gmail.com <steph-nnamani@gmail.com>
2025-04-23 01:00:05 -0500

    added some blocks to variables.tf and main.tf

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -87,6 +87,16 @@ resource "aws_autoscaling_group" "example" {
         value = "${var.cluster_name}-asg"
         propagate_at_launch = true
     }
+
+    dynamic "tag" {
+        for_each = var.custom_tags
+
+        content {
+            key = tag.key
+            value = tag.value
+            propagate_at_launch = true 
+        }
+    }
 }
 
 resource "aws_lb" "example" {
